c3bf795f31a6aaeaab7932cac0e227766a07a0e8
Tracy <75229426+raineytracyn@users.noreply.github.com>
2021-03-14 18:12:42 -0400

    hmwk

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,16 +2,78 @@
 console.log("working");
 // Create the map object with a center and zoom level.
 
-//Set Lat and Long
-Lat = 36.1733
-Long = -120.1794
+// //Set Lat and Long
+// Lat = 36.1733
+// Long = -120.1794
 
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+// Create the map object with center and zoom level.
+// let map = L.map('mapid').setView([30, 30], 2);
 // var map = L.map('mapid').setView([Lat, Long], 13);
 
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+//streets.addTo(map);
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+//access url airport information
+let airportData = "https://raw.githubusercontent.com/raineytracyn/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json"
+
+
 // Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport).addTo(map);
+d3.json(airportData).then(function(data) {
+    console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data, {
+    onEachFeature: function(feature, layer) {
+      console.log(feature);
+      layer.bindPopup("<h2>Airport Code:" + feature.properties.faa + "<h2> <hr> <h3>Airport Name:" + feature.properties.name + "</h3>")}
+  }).addTo(map);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Grabbing our GeoJSON data.
 // L.geoJson(sanFranAirport, {
@@ -20,13 +82,13 @@ L.geoJSON(sanFranAirport).addTo(map);
 //     console.log(feature);
 //     return L.marker(latlng).bindPopup("<h2>" + feature.properties.city + "</h2>");
 //   }
-L.geoJson(sanFranAirport, {
-  // We turn each feature into a marker on the map.
-  onEachFeature: function(feature, layer) {
-    console.log(feature);
-    layer.bindPopup("<h2>City:" + feature.properties.city + "<h2> <hr> <h3>Airport Name:" + feature.properties.name + "</h3>");
-  }
-}).addTo(map);
+// L.geoJson(sanFranAirport, {
+//   // We turn each feature into a marker on the map.
+//   onEachFeature: function(feature, layer) {
+//     console.log(feature);
+//     layer.bindPopup("<h2>City:" + feature.properties.city + "<h2> <hr> <h3>Airport Name:" + feature.properties.name + "</h3>");
+//   }
+// }).addTo(map);
 
 
 
@@ -48,40 +110,36 @@ L.geoJson(sanFranAirport, {
 
 
 
-// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/satellite-streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: API_KEY
-// }).addTo(map);
 
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    //maxZoom: 5,
-    accessToken: API_KEY
-});
+
+// Accessing the airport GeoJSON URL
+// let airportData = "https://raw.githubusercontent.com/raineytracyn/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
+
+// // We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     //maxZoom: 5,
+//     accessToken: API_KEY
+// });
 
 // console.log("working")
 // .addTo (map); 
  
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// // Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
 
-// Get data from cities.js
-let GeoJSON = sanFranAirport;
+// // Get data from cities.js
+// let GeoJSON = sanFranAirport;
 
-// Loop through the cities array and create one marker for each city.
-GeoJSON.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-      radius: city.population/100000
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+// // Loop through the cities array and create one marker for each city.
+// GeoJSON.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location, {
+//       radius: city.population/100000
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//   .addTo(map);
+// });
 
 
 //   //Add Circles
