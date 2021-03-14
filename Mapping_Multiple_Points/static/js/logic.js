fbeebbf858812458a3b90dfbb45ca525b716a6ff
Tracy <75229426+raineytracyn@users.noreply.github.com>
2021-03-14 14:14:50 -0400

    hmwk

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -33,14 +33,11 @@ streets.addTo(map);
 // Get data from cities.js
 let cityData = cities;
 
-// // Loop through the cities array and create one marker for each city.
-// cityData.forEach(function(city) {
-//     console.log(city)
-//    });
-
+// Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
     console.log(city)
-    L.marker(city.location).addTo(map);
+    L.marker(city.location).bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+  .addTo(map);
 });
 
 
