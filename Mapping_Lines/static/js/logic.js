449dac61cc6626118e0f7f2c7e5bbb081f1a8542
Tracy <75229426+raineytracyn@users.noreply.github.com>
2021-03-14 14:48:39 -0400

    hmwk

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -3,24 +3,41 @@ console.log("working");
 // Create the map object with a center and zoom level.
 
 //Set Lat and Long
-Lat = 34.0522
-Long = -118.2437
+Lat = 36.1733
+Long = -120.1794
 
-var map = L.map('mapid').setView([Lat, Long], 13);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+// var map = L.map('mapid').setView([Lat, Long], 13);
+
+// Coordinates for each point to be used in the line.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
+
+
 
 // L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 //     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
+//     id: 'mapbox/satellite-streets-v11',
 //     tileSize: 512,
 //     zoomOffset: -1,
 //     accessToken: API_KEY
 // }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 4,
+    maxZoom: 5,
     accessToken: API_KEY
 });
 
